93c14a9e46b205ec9d06812d9c58ac13af1fc007
cosmob3 <bryanlvelasco@hotmail.com>
2022-04-08 21:17:03 -0600

    Fixing how the file are organized

--- static/api/landingHero.js
+++
@@ -1,9 +0,0 @@
-const landingHero = {
-  title:
-    "Helping people make the world a better place through quality software.",
-  image:
-    "https://res.cloudinary.com/kentcdodds-com/image/upload/w_918,q_auto,f_auto/kentcdodds.com/illustrations/kody-flying_yellow",
-};
-export default () => {
-  return landingHero;
-};
