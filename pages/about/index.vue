e1588d98ee9730a23697983bbd6374fdf6a2f3c2
Will Tengyuan Li <will19940430@gmail.com>
2022-04-08 20:15:17 -0600

    added padding margin to titles

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -5,7 +5,7 @@
         <h1 class="font-mono text-7xl text-orange-500">
           {{ data.companyname }}
         </h1>
-        <h2 class="text-4xl text-orange-200">{{ data.subtitle }}</h2>
+        <h2 class="text-4xl text-orange-200 p-10">{{ data.subtitle }}</h2>
         <div
           class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-5 w-4/5 mx-auto my-10"
         >
@@ -13,7 +13,7 @@
             <img class="rounded-2xl" :src="data.companyimage.filename" alt="" />
           </div>
 
-          <div class="paragraph w-full my-auto mx-auto shadow-xl rounded">
+          <div class="paragraph w-full my-auto mx-auto shadow-xl rounded border-solid border-neutral-100 border-2">
             <p class="font-mono text-2xl leading-normal text-lime-300 p-10">
               {{ data.description }}
             </p>
@@ -23,23 +23,23 @@
       <!-- card-section -->
       <div class="w-full mx-auto my-20 text-center text-white text-3xl">Team members</div>
       <div
-        class="card-section p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-40 text-white"
+        class="card-section p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-40 text-white text-xl"
       >
-        <div class="card1 rounded overflow-hidden shadow-lg">
+        <div class="card1 rounded overflow-hidden shadow-lg border-solid border-neutral-100 border-2">
           <div class="image-warpper w-full max-h-80 overflow-hidden">
             <img class="object-cover" :src="data.tm1photo.filename" alt="" />
           </div>
           <h2>{{ data.tm1name }}</h2>
           <p>{{ data.tm1info }}</p>
         </div>
-        <div class="card2 rounded overflow-hidden shadow-lg">
+        <div class="card2 rounded overflow-hidden shadow-lg border-solid border-neutral-100 border-2">
           <div class="image-warpper w-full max-h-80 overflow-hidden">
             <img class="w-full" :src="data.tm2photo.filename" alt="" />
           </div>
           <h2>{{ data.tm2name }}</h2>
           <p>{{ data.tm2info }}</p>
         </div>
-        <div class="card3 rounded overflow-hidden shadow-lg">
+        <div class="card3 rounded overflow-hidden shadow-lg border-solid border-neutral-100 border-2">
           <div class="image-warpper w-full max-h-80 overflow-hidden">
             <img class="w-full" :src="data.tm3photo.filename" alt="" />
           </div>
@@ -49,7 +49,7 @@
       </div>
       <!-- address cell email -->
       <div class="w-full mx-auto mt-20 text-center text-white text-3xl">Meet Us!</div>
-      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-40 py-40">
+      <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-40 py-40 mx-20">
         <div class="address flex">
           <img class="w-1/5" src="assets\images\addressimage.jpg" alt="">
           <h1 class="text-white w-full m-auto px-8">{{data.address}}</h1>
